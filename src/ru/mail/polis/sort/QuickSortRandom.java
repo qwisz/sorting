3585914e8014c7e9c887ce50b0d9b44c6ec1b1a7
qwisz <barybin.anton@gmail.com>
2016-11-27 22:29:28 +0300

    add results

--- src/ru/mail/polis/sort/QuickSortRandom.java
+++ src/ru/mail/polis/sort/QuickSortRandom.java
@@ -17,7 +17,6 @@ public class QuickSortRandom {
         int l = b, r = e;
         Random rnd = new Random();
         int index = l + rnd.nextInt(r - l + 1);
-//        int piv = arr[(l + r) / 2]; // Опорным элементом для примера возьмём средний
         int piv = arr[index];
         while (l <= r)
         {
@@ -33,44 +32,6 @@ public class QuickSortRandom {
             qsort (arr, b, r);
         if (e > l)
             qsort (arr, l, e);
-    }    /* ----- end of function qsort ----- */
+    }
 
-//    private static void quicksort(int[] a, int l, int r) {
-//        if (r <= l)
-//            return;
-//        Random rnd = new Random();
-//        int index = l + rnd.nextInt(r - l + 1);
-//        int v = a[index];
-//        Helper.swap(a, index, r - 1);
-//        int i = l;
-//        int j = r - 1;
-//        int p = l - 1;
-//        int q = r;
-//        while (true) {
-//            while (a[i] < v)
-//                i++;
-//            while (a[j] > v)
-//                j--;
-//            if (i >= j)
-//                break;
-//            Helper.swap(a, a[i], a[j]);
-//            if (a[i] == v) {
-//                p++;
-//                Helper.swap(a, a[p], a[i]);
-//            }
-//            if (a[j] == v) {
-//                q--;
-//                Helper.swap(a, a[q], a[j]);
-//            }
-//        }
-//        Helper.swap(a, a[i], a[r]);
-//        j = i - 1;
-//        i++;
-//        for (int k = 0; k <= p; k++, j--)
-//            Helper.swap(a, a[k], a[j]);
-//        for (int k = r - 1; k >= q; k--, i++)
-//            Helper.swap(a, a[k], a[i]);
-//        quicksort(a, 0, j);
-//        quicksort(a, i, r);
-//    }
 }
